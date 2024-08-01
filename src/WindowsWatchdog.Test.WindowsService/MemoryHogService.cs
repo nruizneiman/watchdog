47085377ba68279ddc26e25e97f1b4eff034c012
Nicolas Ruiz Neiman <nicolasruizneiman@gmail.com>
2024-08-01 17:32:16 -0300

    Add windows services

---
+++ src/WindowsWatchdog.Test.WindowsService/MemoryHogService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Timers;
+using System.Collections.Generic;
+
+namespace WindowsWatchdog.Test.WindowsService
+{
+    public partial class MemoryHogService : ServiceBase
+    {
+        private Timer timer;
+        private List<byte[]> memoryHogList;
+
+        public MemoryHogService()
+        {
+            InitializeComponent();
+
+            memoryHogList = new List<byte[]>();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer();
+            timer.Interval = 10000; // 10 seconds
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            memoryHogList.Clear();
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            // Allocate 1 GB of memory
+            byte[] memoryChunk = new byte[1024 * 1024 * 1024];
+            memoryHogList.Add(memoryChunk);
+            // Fill the memory with some data
+            for (int i = 0; i < memoryChunk.Length; i++)
+            {
+                memoryChunk[i] = 0xFF;
+            }
+        }
+    }
+}
